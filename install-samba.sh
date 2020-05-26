2721ad09a85d36bb1be796672d35ddd0b105f0f8
pzhaoyang <pzhaoyang@126.com>
2020-05-26 22:12:35 +0800

    modify samba

--- install-samba.sh
+++ install-samba.sh
@@ -3,16 +3,12 @@
 docker rm -f samba
 docker run -d \
 --name samba \
--e USERID=65534 \
--e GROUPID=65534 \
+-e USERID=0 \
+-e GROUPID=0 \
 -p 139:139 \
 -p 445:445 \
--v /srv/mediacenter/disk1:/share1 \
--v /srv/mediacenter/disk2:/share2 \
--v /srv/mediacenter/disk3:/share3 \
+-v /srv/mediacenter/disk1/downloads:/share \
 --restart=unless-stopped \
-freesmall/samba -p \
+dperson/samba -p \
 -u "nobody;badpass;" \
--s "mediacenter1;/share1;yes;no;yes;all;none;;RaspSamba" \
--s "mediacenter2;/share2;yes;no;yes;all;none;;RaspSamba" \
--s "mediacenter3;/share3;yes;no;yes;all;none;;RaspSamba"
+-s "mediacenter;/share;yes;no;yes;all;none;;RaspSamba"
