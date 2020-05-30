9bdd1c44bde274e5e00aaff0d3b803e8c7bc0a90
pzhaoyang <pzhaoyang@126.com>
2020-05-30 21:26:31 +0800

    add shairport

---
+++ install-shairport.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+docker rm -f shairport > /dev/null
+docker run -d \
+--name shairport
+--net host \
+--device /dev/snd \
+-e AIRPLAY_NAME=HomePod-$(hostname) \
+freesmall/shairport-sync
